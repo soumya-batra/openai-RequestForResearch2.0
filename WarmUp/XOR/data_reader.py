1e0f94e26245357450f243fbdf9601da883256f0
Soumya Batra <sobatr@microsoft.com>
2018-02-15 03:27:36 -0800

    Adding Data Reader

---
+++ WarmUp/XOR/data_reader.py
@@ -0,0 +1,39 @@
+import pickle
+import sys
+
+class DataReader():
+    def __init__(self):
+        self.sequences = []
+        self.labels = []
+
+    def read_pkl_data_at_file_path(self, filepath):       
+        with open(filepath, 'rb') as fptr:
+            data = pickle.load(fptr)
+        self.sequences = data['sequences']
+        self.labels = data['labels']
+
+    def get_sequences(self):
+        return self.sequences
+
+    def get_labels(self):
+        return self.labels
+
+
+def run(sysargs):
+    dr = DataReader()
+
+    input_file_path = sysargs[0]
+    dr.read_pkl_data_at_file_path(input_file_path)
+
+    sequences = dr.get_sequences()
+    labels = dr.get_labels()
+
+    print('Average Length = ', sum([len(sequence) for sequence in sequences])/len(sequences))
+    print('Minimum Length = ', min([len(sequence) for sequence in sequences]))
+    print('Maximum Length = ', max([len(sequence) for sequence in sequences]))
+
+    print('Number of odd parity = ', sum(1 for label in labels if label == 1))
+    print('Number of even parity = ', sum(1 for label in labels if label == 0))
+
+if __name__ == '__main__':
+    run(sys.argv[1:])
